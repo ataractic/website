b1073d925e59055a13682510d83c72243eb0f7f3
eretsym <contact@myst.re>
2024-02-04 17:49:15 +0100

    Add margin on top of the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div class="flex flex-col justify-center max-w-screen-md mx-auto">
+    <div class="flex flex-col justify-center max-w-screen-md mx-auto mt-3">
       <div class="flex gap-3 p-1 text-white justify-between border border-[#393E59] bg-[#222625]">
         <div>copyright (c) 2024 - ataractic</div>
         <a class="text-[#ECF2BD] underline" href="https://fresh.deno.dev/">
