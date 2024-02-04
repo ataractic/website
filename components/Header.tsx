c526adef2e5a5d9f5b4eefd7d7c371e26d7dc393
eretsym <contact@myst.re>
2024-02-04 17:49:37 +0100

    Update header css justificatoon

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,20 +3,22 @@ import { Logo } from "../islands/Logo.tsx";
 export function Header() {
   return (
     <div class="flex flex-col justify-center max-w-screen-md mx-auto mb-3">
-      <div class="flex gap-3 p-1 text-white justify-center border border-[#393E59] bg-[#222625]">
+      <div class="flex gap-3 p-1 text-white justify-between border border-[#393E59] bg-[#222625]">
         <Logo />
-        <a class="text-[#ECF2BD] underline" href="/">
-          home
-        </a>
-        <a class="text-[#ECF2BD] underline" href="/music">
-          music
-        </a>
-        <a class="text-[#ECF2BD] underline" href="/contact">
-          contact
-        </a>
-        <a class="text-[#ECF2BD] underline" href="/projects">
-          projects
-        </a>
+        <div class="flex gap-3">
+          <a class="text-[#ECF2BD] underline" href="/">
+            home
+          </a>
+          <a class="text-[#ECF2BD] underline" href="/music">
+            music
+          </a>
+          <a class="text-[#ECF2BD] underline" href="/contact">
+            contact
+          </a>
+          <a class="text-[#ECF2BD] underline" href="/projects">
+            projects
+          </a>
+        </div>
       </div>
     </div>
   );
