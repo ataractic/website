0736422d08a822152dc88d588b1ccc7a05c0f981
ataractic <ataractic@pm.me>
2024-02-19 22:44:37 +0100

    Add website iteration in the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <div class="flex flex-col justify-center max-w-screen-md mx-auto mt-3">
       <div class="bg-gradient-to-t from-[#99CCFF] to-[#FFFFFF] border border-[#99CCFF] flex rounded-md justify-between px-4 py-1 mb-2">
-        <div>copyright (c) 2024 - ataractic</div>
+        <div><b>v2</b> - copyright (c) 2024 - ataractic</div>
         <a class="text-[#FFFFFF] underline" href="https://fresh.deno.dev/">
           made with fresh
         </a>
