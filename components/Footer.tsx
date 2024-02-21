0e5a7c35501432ea51cad924077cb4458b92259b
ataractic <ataractic@pm.me>
2024-02-21 10:46:04 +0100

    Add link to source in the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,15 @@ export function Footer() {
     <div class="flex flex-col justify-center mx-auto mt-2">
       <div class="bg-gradient-to-t from-[#99CCFF] dark:from-[#000000] to-[#FFFFFF] dark:to-[#333333] border border-[#99CCFF] dark:border-[#000000] flex rounded-md justify-between px-2 py-1 mb-2">
         <div><b>v2</b> - copyright (c) 2024 - ataractic</div>
+        <div>
+        <a href="https://github.com/ataractic/website">
+          source
+        </a>
+        <span> - </span>
         <a href="https://fresh.deno.dev/">
           made with fresh
         </a>
+        </div>
       </div>
     </div>
   );
